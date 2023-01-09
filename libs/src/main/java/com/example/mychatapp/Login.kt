8f4c6fe8a87d0b9216ed2eb5be710eefaaf97a94
marshvin <vincentmarsh01@gmail.com>
2023-01-09 21:20:41 +0300

    log in page

---
+++ libs/src/main/java/com/example/mychatapp/Login.kt
@@ -0,0 +1,32 @@
+package com.example.mychatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class Login : AppCompatActivity() {
+
+    private lateinit var edtEmail:EditText
+    private lateinit var edtPassword:EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignup:Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        edtEmail=findViewById(R.id.edt_Email)
+        edtPassword=findViewById(R.id.edt_Password)
+        btnLogin=findViewById(R.id.btn_Login)
+        btnSignup=findViewById(R.id.btn_Signup)
+
+        btnSignup.setOnClickListener{
+            val intent= Intent(this, Signup::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
